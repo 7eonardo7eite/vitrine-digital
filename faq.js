65a28dca2116a68cfb789b6735c040e521f97b7e
7eonardo7eite <leonardo.mleitte@gmail.com>
2026-06-28 01:32:16 -0300

    ajustes de responsividade e refatoração semântica

--- faq.js
+++ faq.js
@@ -1,18 +1,18 @@
 const faqItems = document.querySelectorAll(".faq-item");
 
 faqItems.forEach(item => {
-    const question = item.querySelector(".faq-question");
+    const question = item.querySelector(".faq-item__question");
 
     question.addEventListener("click", () => {
-        const answer = item.querySelector(".faq-answer");
-        const arrow = item.querySelector(".arrow img");
+        const answer = item.querySelector(".faq-item__answer");
+        const arrow = item.querySelector(".faq-item__icon img");
 
         // Fecha os outros
         faqItems.forEach(i => {
             if (i !== item) {
                 i.classList.remove("active");
-                i.querySelector(".faq-answer").style.maxHeight = null;
-                i.querySelector(".arrow img").style.transform = "rotate(0deg)";
+                i.querySelector(".faq-item__answer").style.maxHeight = null;
+                i.querySelector(".faq-item__icon img").style.transform = "rotate(0deg)";
             }
         });
 
